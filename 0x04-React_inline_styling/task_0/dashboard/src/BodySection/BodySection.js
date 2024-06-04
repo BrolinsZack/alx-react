bed4c09771c564cc32c26d845268d4d783f99710
BrolinsZack <zackbrolins@gmail.com>
2024-06-04 19:23:36 +0000

    statuses

---
+++ 0x04-React_inline_styling/task_0/dashboard/src/BodySection/BodySection.js
@@ -0,0 +1,15 @@
+mport React from 'react'
+
+
+class BodySection extends React.Component{
+    render(){
+        const {children,title} = this.props
+        return(
+        <div className="bodySection">
+            <h2>{title}</h2>
+            {children}
+        </div>
+        )
+    }
+}
+
